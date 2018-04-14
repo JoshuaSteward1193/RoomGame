205efe8385719240692499a56eda19480ee66435
Joshua Steward <31291387+JoshuaSteward1193@users.noreply.github.com>
2018-04-13 21:02:37 -0500

    work

---
+++ RoomGame/Consumable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomGame
+{
+    abstract class Consumable : Item
+    {
+        public string UseText { get; set; }
+        virtual public void use()
+        {
+
+        }
+        
+    }
+}
---
+++ RoomGame/Potion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomGame
+{
+    
+    class Potion : Consumable
+    {
+        public int Strength { get; set; }
+        public enum Type { healing, poision, resting }
+        public Type PotionType;
+        public Potion(string n, string pn, string ft, string ut, Type pt, int s)
+        {
+            Name = n;
+            PluralName = pn;
+            FlavorText = ft;
+            UseText = ut;
+            PotionType = pt;
+            Strength = s;
+        }
+
+        override public void use()
+        {
+            switch (PotionType)
+            {
+                case Type.healing:
+                    PlayerData.gainHealth(Strength);
+                    break;
+                case Type.resting:
+                    //Restores player stamina
+                    break;
+                case Type.poision:
+                    //Damages player health
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
